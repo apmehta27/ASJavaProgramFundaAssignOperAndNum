eb01bcfc14acdd56c7a1a7b39bac5eda35ad9862
Alpa Mehta <apmehta27@gmail.com>
2021-09-27 16:30:13 -0500

    init 2nd assignment submission.

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*Problem 2:Convert the following binary numbers to decimal notation.
+        Do not use any Java functions or online conversion applications to calculate
+        the answer as this will hinder the learning process and your understanding of the concept.
+                However, you may check your answers using Java methods.
+        0010 -2
+        1001 - 9
+        0011 0100 -52
+        0111 0010 - 114
+        0010 0001 1111 - 543
+        0010 1100 0110 0111- 11367
+
+        Write a program that declares an integer variable, assigns a number, and uses a postfix
+        increment operator to increase the value. Print the value before and after the increment
+        operator.
+*/
+
+
+    }
+}
